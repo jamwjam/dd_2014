8be632dd4ad1b9a15d89529781ac8446d7a485e6
stevarn <stevarn@gmail.com>
2014-09-13 19:21:19 +0000

    footer css

---
+++ head.php
@@ -0,0 +1,12 @@
+<!-- Insert all links in this file to include in all other html files -->
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+<link type="text/css" rel="stylesheet" href="template.css"/>
--- index.php
+++ index.php
@@ -4,10 +4,18 @@ To change this template use Tools | Templates.
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-    <title></title>
+    <?php include ('head.php'); ?>
+    <title>Toaster</title>
 </head>
 <body>
-    <h1>Hello World!</h1>
-    <a href="login">Login here</a>
+	<div id="wrapper">
+		<div id="header">
+            <h1>TITLE</h1>
+        </div>
+		<div id="content"></div>
+		<div id="footer">
+            <h1>FOOTER</h1>
+        </div>
+	</div>
 </body>
 </html>
