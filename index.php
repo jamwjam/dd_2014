9ae1882026a6ef0c725290a1c7c0cda8bdd57c6d
stevarn <stevarn@gmail.com>
2014-09-14 06:48:45 +0000

    again mysql7

--- index.php
+++ index.php
@@ -16,12 +16,15 @@
     } 
     else 
     {
-        mysql_query("CREATE TABLE example(
-        id INT NOT NULL AUTO_INCREMENT, 
-        PRIMARY KEY(id),
-         name VARCHAR(30), 
-         age INT)")
-         or die(mysql_error());  
+        // Create table
+        $sql="CREATE TABLE Persons(FirstName CHAR(30),LastName CHAR(30),Age INT)";
+
+        // Execute query
+        if (mysqli_query($con,$sql)) {
+          echo "Table persons created successfully";
+        } else {
+          echo "Error creating table: " . mysqli_error($con);
+        }
     }
 
 ?>
